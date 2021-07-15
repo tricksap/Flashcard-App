fbe208b3f4ab7df16c9e9c4062d89849df7e8c55
Patrick <tricksap@gmail.com>
2021-07-15 16:42:27 +0800

    add header and card component

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import Card from "./Card";
+import Header from "./Header";
 function App() {
   return (
     <div>
+      <Header />
       <Card />
     </div>
   );
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import classes from "./index.module.css";
+function Header() {
+  return (
+    <header className={classes.header}>
+      <h1>FlashCards</h1>
+    </header>
+  );
+}
+
+export default Header;
